7de1cd55c5cfc6376ca5186a9d355dc57b40b730
tweebaajin <cxjinchen@gmail.com>
2021-04-16 17:13:17 -0400

    @LoadBalanced and http://CLOUD-PAYMENT-SERVICE

--- doc/backup.ps1
+++ doc/backup.ps1
@@ -10,6 +10,23 @@ Copy-Item C:\Users\JinTweebaa\Documents\tweebaa\cloud\test-sql.sql C:\Users\JinT
 # done
 # mkdir C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\doc\
 
+Remove-Item -Path "C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-api-commons\target" -recurse -Force
+Remove-Item -Path "C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-api-commons\*.iml" -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-consumer-order80\target -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-consumer-order80\*.iml -recurse -Force
+
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-eureka-server7001\target -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-eureka-server7001\*.iml -recurse -Force
+
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-eureka-server7002\target -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-eureka-server7002\*.iml -recurse -Force
+
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-provider-payment8001\target -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-provider-payment8001\*.iml -recurse -Force
+
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-provider-payment8002\target -recurse -Force
+Remove-Item -Path C:\Users\JinTweebaa\Documents\tweebaa\cloud\cloud202104github\cloud\cloud-provider-payment8002\*.iml -recurse -Force
+
 cd C:/Users/JinTweebaa/Documents/tweebaa/cloud/cloud202104github/cloud
 
 git status
