ef2b780c1abc39d8e2ac65172576ca53ef677de8
tweebaajin <cxjinchen@gmail.com>
2021-05-17 00:27:11 -0400

    RT flow limit

--- cloudalibaba-sentinel-service8401/src/main/java/springcloud/controller/FollowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/springcloud/controller/FollowLimitController.java
@@ -1,11 +1,13 @@
 package springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FollowLimitController {
     @GetMapping("/testA")
     public String testA(){
@@ -19,6 +21,15 @@ public class FollowLimitController {
     }
     @GetMapping("/testB")
     public  String testB(){
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "-----testB";
+    }
+    @GetMapping("/testD")
+    public String testD(){
+        //try{TimeUnit.SECONDS.sleep(1);}catch(InterruptedException e){e.printStackTrace();}
+        //log.info("testD test RT");
+        log.info("testD 异常比例");
+        int age = 10/0;
+        return "------testD";
     }
 }
