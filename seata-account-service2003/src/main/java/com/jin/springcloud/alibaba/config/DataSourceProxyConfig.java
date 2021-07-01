ff86ef920fd852da64ac448e29959f77ef3ba7dd
tweebaajin <cxjinchen@gmail.com>
2021-07-01 01:34:55 -0400

    @Bean public DataSourceProxy dataSourceProxy(DataSource dataSource){return new DataSourceProxy(dataSource);}

--- seata-account-service2003/src/main/java/com/jin/springcloud/alibaba/config/DataSourceProxyConfig.java
+++ seata-account-service2003/src/main/java/com/jin/springcloud/alibaba/config/DataSourceProxyConfig.java
@@ -1,8 +1,10 @@
 package com.jin.springcloud.alibaba.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -24,18 +26,18 @@ public class DataSourceProxyConfig {
         return new DruidDataSource();
     }
 
-//    @Bean
-//    public DataSourceProxy dataSourceProxy(DataSource dataSource){
-//        return new DataSourceProxy(dataSource);
-//    }
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
-        return factoryBean.getObject();
+    public DataSourceProxy dataSourceProxy(DataSource dataSource){
+        return new DataSourceProxy(dataSource);
     }
-    /*
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+//        factoryBean.setDataSource(dataSource);
+//        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+//        return factoryBean.getObject();
+//    }
+
     @Bean
     public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
@@ -44,6 +46,7 @@ public class DataSourceProxyConfig {
         sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
         return sqlSessionFactoryBean.getObject();
     }
+    /*
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
