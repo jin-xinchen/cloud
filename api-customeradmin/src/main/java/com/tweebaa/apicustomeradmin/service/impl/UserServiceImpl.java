98c4c0df71f0820f31113544767b45031f2ccb48
tweebaajin <cxjinchen@gmail.com>
2021-08-09 23:38:35 -0400

    Environment

--- api-customeradmin/src/main/java/com/tweebaa/apicustomeradmin/service/impl/UserServiceImpl.java
+++ api-customeradmin/src/main/java/com/tweebaa/apicustomeradmin/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
-package com.tweebaa.apicustomeradmin.service.impl;
+package com.Test.apicustomeradmin.service.impl;
 
-import com.tweebaa.apicustomeradmin.controller.PageCustomers;
-import com.tweebaa.apicustomeradmin.entity.UserA;
-import com.tweebaa.apicustomeradmin.service.UserService;
+import com.Test.apicustomeradmin.controller.PageCustomers;
+import com.Test.apicustomeradmin.entity.UserA;
+import com.Test.apicustomeradmin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.sql.*;
@@ -11,6 +13,11 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    public void setEnv(Environment pEnv){
+        env = pEnv;
+    }
+    private Environment env;
     @Override
     public PageCustomers receiveUsersJDBC(int currPage, int pageSize) {
         int pagesize =pageSize;
@@ -25,10 +32,23 @@ public class UserServiceImpl implements UserService {
 //        users.add(user1);
         Connection Conn=null;
         try{
+
             String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-            String conn="jdbc:sqlserver://localhost:1433;DatabaseName=testSNAP";
-            String username="test_dev";
+            String conn="jdbc:sqlserver://localhost:1433;DatabaseName=TestSNAP";
+            String username="Test_dev";
             String passW="%gM$VISyPjwhx%tMD2";
+            String keyValue = env.getProperty("mssql.datasource.driver-class-name");
+            if( keyValue!= null && !keyValue.isEmpty())
+            {
+                driver = keyValue;
+            }
+            keyValue = env.getProperty("mssql.datasource.url");
+            if( keyValue!= null && !keyValue.isEmpty())
+            {
+                conn = keyValue;
+            }
+            username = env.getProperty("mssql.datasource.username");
+            passW =  env.getProperty("mssql.datasource.password");
             Conn= DriverManager.getConnection(conn,username,passW);
             Statement stmt=Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
             String sql1 = "select count(c.Id) from Customer as c inner join Store as s on s.Id = c.Store_Id and s.OwnerId = c.Id left join Download as d on d.id = c.ThumbnailAvatarDownloadId where CountryCallingCode is not null and PhoneNumber is not null and c.Deleted != 1";
@@ -47,17 +67,17 @@ public class UserServiceImpl implements UserService {
             rs.close();
             List<UserA> users = new ArrayList<UserA>();
             int pageIndex = pageNow+1;
-            String sql = "select c.Id,c.Username,ROW_NUMBER() over(order by c.CreatedOnUtc desc) as rn from Customer as c inner join Store as s on s.Id=c.Store_Id and s.OwnerId=c.Id left join Download as d on d.id = c.ThumbnailAvatarDownloadId where CountryCallingCode is not null and PhoneNumber is not null and c.Deleted != 1";
+            String sql = "select c.Id,ROW_NUMBER() over(order by c.CreatedOnUtc desc) as rn from Customer as c inner join Store as s on s.Id=c.Store_Id and s.OwnerId=c.Id left join Download as d on d.id = c.ThumbnailAvatarDownloadId where CountryCallingCode is not null and PhoneNumber is not null and c.Deleted != 1";
             String query = "select * from ("+sql+") p where rn>"+(pageIndex-1)*pagesize +" and rn<="+pageIndex*pagesize;
             rs=stmt.executeQuery(query);
             while (rs.next()) {
                 UserA userA = new UserA();
                 userA.setUserId(rs.getLong("Id"));
-                userA.setUserName(rs.getString("Username"));
                 users.add(userA);
             }
             pageCustomers.setList(users);
             rs.close();
+            stmt.close();
             Conn.close();
         }
         catch(Exception e){
